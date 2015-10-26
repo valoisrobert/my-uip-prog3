39c1aa9d408ab57f74245cd741183a817909fbde
valoisrobert <valoisrobert@gmail.com>
2015-10-26 15:43:14 -0700

    quiz5

---
+++ LABORATORIO/Qhola#5.py
@@ -0,0 +1,25 @@
+
+
+
+class hola(object):
+
+
+def(self):
+self.mensaje = mensaje
+self.entidad = entidad
+     
+def mostrar_entidad(self.entidad)
+     print("LA ENTIDAD  ES: "  +str(self.entidad))
+	 
+	 
+def comparar_mensaje (mensaje)	 
+    if  mensaje ==("hola mundo")
+   print("mensaje es igual a  hola mundo")
+   
+   else 
+         ("el  mensaje  no es igual a hola mundo")
+   
+   def  guardar mensaje(mensaje,archivo)
+   archivo = open(archivo, "w")
+   
+   nuevo.close()
---
+++ LABORATORIO/quiz#5.py
@@ -0,0 +1,16 @@
+
+
+
+class hola:
+
+
+def__init__(self,entidad, mensaje)
+self.mensaje = mensaje
+self.entidad = entidad
+     
+def mostrar_entidad(self)
+     print("LA ENTIDAD  ES: "  +str(self.entidad))
+	 
+	 
+def comparar_mensaje ()	 
+    if  mensaje 
